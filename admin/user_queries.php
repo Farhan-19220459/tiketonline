bbf556a264c1855d3225f0ab6bc4d65abb286637
randi089 <randifebriadi@gmail.com>
2024-07-02 08:43:42 +0700

    fix contatc dan delete queries

--- admin/user_queries.php
+++ admin/user_queries.php
@@ -1,63 +1,96 @@
 <?php
-    require('views/essentials.php');
-    require('views/db_config.php');
-    adminLogin();
+require('views/essentials.php');
+require('views/db_config.php');
+adminLogin();
 
-    if(isset($_GET['seen']))
-    {
-        $frm_data = filteration($_GET);
-        
-        if($frm_data['seen']=='all'){
+if (isset($_GET['seen'])) {
+    $frm_data = filteration($_GET);
+
+    if ($frm_data['seen'] == 'all') {
         $q = "UPDATE `user_queries` SET `seen`=?";
         $values = [1];
-        if(update($q,$values,'i')){
-                alert('success','Telah Dibaca!');
-            }
-            else{
-                alert('error','Gagal Terbaca!');
-            }
+        if (update($q, $values, 'i')) {
+            alert('success', 'Telah Dibaca!');
+        } else {
+            alert('error', 'Gagal Terbaca!');
         }
-        else{
-            $q = "UPDATE `user_queries` SET `seen`=? WHERE `no`=?";
-            $values = [1,$frm_data['seen']];
-            if(update($q,$values,'ii')){
-                alert('success','Telah Dibaca!');
-            }
-            else{
-                alert('error','Gagal Terbaca!');
-            }
+    } else {
+        $q = "UPDATE `user_queries` SET `seen`=? WHERE `no`=?";
+        $values = [1, $frm_data['seen']];
+        if (update($q, $values, 'ii')) {
+            alert('success', 'Telah Dibaca!');
+        } else {
+            alert('error', 'Gagal Terbaca!');
+        }
+    }
+}
+
+// if (isset($_GET['del'])) {
+//     $frm_data = filteration($_GET);
+
+//     if ($frm_data['del'] == 'all') {
+//         $q = "DELETE FROM `user_queries`";
+//         $values = [$frm_data['del']];
+//         if (delete($q, $values, 'i')) {
+//             alert('success', 'Pesan Telah Dihapus!');
+//         } else {
+//             alert('error', 'Gagal!');
+//         }
+//     } else {
+//         $q = "DELETE `user_queries` WHERE `no`=?";
+//         $values = [$frm_data['del']];
+//         if (delete($q, $values, 'i')) {
+//             alert('success', 'Pesan Telah Dihapus!');
+//         } else {
+//             alert('error', 'Gagal!');
+//         }
+//     }
+// }
+
+if (isset($_GET['del'])) {
+
+    if ($_GET['del'] == 'all') {
+        $query = "DELETE FROM user_queries";
+        if (delete($query) > 0) {
+            alert('success', 'Semua Pesan Telah Dihapus!');
+        } else {
+            alert('error', 'Gagal!');
+        }
+    } else {
+        $no = $_GET['del'];
+        $query = "DELETE FROM user_queries WHERE no = $no";
+        if (delete($query) > 0) {
+            alert('success', 'Pesan Telah Dihapus!');
+        } else {
+            alert('error', 'Gagal!');
         }
     }
 
-    if(isset($_GET['del']))
-    {
-        $frm_data = filteration($_GET);
-        
-        if($frm_data['del']=='all'){
-            $q = "DELETE FROM `user_queries`";
-            $values = [$frm_data['del']];
-            if(update($q,$values,'i')){
-                alert('success','Pesan Telah Dihapus!');
-            }
-            else{
-                alert('error','Gagal!');
-            }
-        }
-        else{
-            $q = "DELETE `user_queries` WHERE `no`=?";
-            $values = [$frm_data['del']];
-            if(update($q,$values,'i')){
-                alert('success','Pesan Telah Dihapus!');
-            }
-            else{
-                alert('error','Gagal!');
-            }
-        }
-    }
+    // $frm_data = filteration($_GET);
+
+    // if ($frm_data['del'] == 'all') {
+    //     $q = "DELETE FROM `user_queries`";
+    //     $values = [$frm_data['del']];
+    //     if (delete($q, $values, 'i')) {
+    //         alert('success', 'Pesan Telah Dihapus!');
+    //     } else {
+    //         alert('error', 'Gagal!');
+    //     }
+    // } else {
+    //     $q = "DELETE `user_queries` WHERE `no`=?";
+    //     $values = [$frm_data['del']];
+    //     if (delete($q, $values, 'i')) {
+    //         alert('success', 'Pesan Telah Dihapus!');
+    //     } else {
+    //         alert('error', 'Gagal!');
+    //     }
+    // }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -65,57 +98,57 @@
     <title>Admin Panel - User Queries</title>
     <?php require('views/links.php'); ?>
 </head>
+
 <body class="bg-light">
 
     <?php require('views/header.php'); ?>
 
-<div class="container-fluid" id="main-content">
-    <div class="row">
-        <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-            <h3 class="mb-4">User Queries</h3>
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                <h3 class="mb-4">User Queries</h3>
 
-            <div class="card border-0 shadow-sm mb-4">
-                <div class="card-body">
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
 
-                <div class="text-end mb-4">
-                    <a href="?seen=all" class="btn btn-dark rounded-pill shadow-none btn-sm">
-                        <i class="bi bi-check-all"></i> Tandai Semua Telah Dibaca
-                    </a>
-                </div>
-                <div class="text-end mb-4">
-                    <a href="?del=all" class="btn btn-danger rounded-pill shadow-none btn-sm">
-                    <i class="bi bi-trash"></i> Hapus Semua
-                    </a>
-                </div>
+                        <div class="text-end mb-4">
+                            <a href="?seen=all" class="btn btn-dark rounded-pill shadow-none btn-sm">
+                                <i class="bi bi-check-all"></i> Tandai Semua Telah Dibaca
+                            </a>
+                        </div>
+                        <div class="text-end mb-4">
+                            <a href="?del=all" class="btn btn-danger rounded-pill shadow-none btn-sm">
+                                <i class="bi bi-trash"></i> Hapus Semua
+                            </a>
+                        </div>
 
-                    <div class="table-responsive-md" style="height: 450px; overflow-y: scroll;">
-                        <table class="table table-hover border">
-                            <thead class="sticky-top">
-                                <tr class="bg-dark text-light">
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Subject</th>
-                                <th scope="col">Message</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
+                        <div class="table-responsive-md" style="height: 450px; overflow-y: scroll;">
+                            <table class="table table-hover border">
+                                <thead class="sticky-top">
+                                    <tr class="bg-dark text-light">
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Subject</th>
+                                        <th scope="col">Message</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
                                     $q = "SELECT * FROM `user_queries` ORDER BY `no` DESC";
-                                    $data = mysqli_query($conn,$q);
-                                    $i=1;
+                                    $data = mysqli_query($conn, $q);
+                                    $i = 1;
 
-                                    while($row = mysqli_fetch_assoc($data))
-                                    {
-                                        $seen='';
-                                        if($row['seen']!=1){
+                                    while ($row = mysqli_fetch_assoc($data)) {
+                                        $seen = '';
+                                        if ($row != 1) {
                                             $seen = "<a href='?seen=$row[no]' class='btn btn-sm rounded-pill btn-primary'>Mark as Read!</a>";
                                         }
-                                        $seen.= "<a href='?del=$row[no]' class='btn btn-sm rounded-pill btn-danger mt-2'>Delete!</a>";
+                                        $seen .= "<a href='?del=$row[no]' class='btn btn-sm rounded-pill btn-danger mt-2'>Delete!</a>";
 
-                                        echo<<<query
+                                        echo <<<query
                                             <tr>
                                                 <td>$i</td>
                                                 <td>$row[name]</td>
@@ -128,19 +161,19 @@
                                         query;
                                         $i++;
                                     }
-                                ?>
-                            </tbody>
-                        </table>
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
-</div>
-    
-<?php require('views/scripts.php') ?>
+
+    <?php require('views/scripts.php') ?>
 
 
 </body>
--- class.php
+++ class.php
@@ -1,128 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Class</title>
-  <?php require('views/links.php'); ?>
+  <?= require('views/links.php'); ?>
 </head>
+
 <body class="bg-light">
-  
-<?php require('views/header.php'); ?>
 
-<div class="my-5 px-4">
-  <h2 class="fw-bold h-font text-center">Class</h2>
-  <div class="h-line bg-dark"></div>
-</div>
+  <?= require('views/header.php'); ?>
 
-<div class="container-fluid">
-  <div class="row">
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">Class</h2>
+    <div class="h-line bg-dark"></div>
+  </div>
 
-    <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 ps-4">
-      <nav class="navbar navbar-expand navbar-light bg-light bg-white rounded shadow">
-        <div class="container-fluid flex-lg-column align-items-stretch">
-          <h4 class="mt-2">Filters</h4>
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 ps-4">
+        <nav class="navbar navbar-expand navbar-light bg-light bg-white rounded shadow">
+          <div class="container-fluid flex-lg-column align-items-stretch">
+            <h4 class="mt-2">Filters</h4>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#filtersDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
-                </button>
-              <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="filterDropdown">
-            <div class="border bg-light p-3 rounded mb-3">
-              <h5 class="mb-3" style:"font-size: 18px;">Cek Ketersediaan</h5>
-              <label class="form-label">Dari</label>
-              <input type="date" class="form-control shadow-none mb-3">
-              <label class="form-label">Ke</label>
-              <input type="date" class="form-control shadow-none">
-            </div>
-            <div class="border bg-light p-3 rounded mb-3">
-              <h5 class="mb-3" style:"font-size: 18px;">Fasilitas</h5>
-              <div class="mb-2">
-                <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
-                <label class="form-check-label" for="f1">AC</label>
+            </button>
+            <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="filterDropdown">
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style:"font-size: 18px;">Cek Ketersediaan</h5>
+                <label class="form-label">Dari</label>
+                <input type="date" class="form-control shadow-none mb-3">
+                <label class="form-label">Ke</label>
+                <input type="date" class="form-control shadow-none">
               </div>
-              <div class="mb-2">
-                <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
-                <label class="form-check-label" for="f2">Stopkontak</label>
-              </div>
-              <div class="mb-2">
-                <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
-                <label class="form-check-label" for="f4">Wifi</label>
-              </div>
-              <div class="mb-2">
-                <input type="checkbox" id="f4" class="form-check-input shadow-none me-1">
-                <label class="form-check-label" for="f5">Selimut</label>
-              </div>
-              <div class="mb-2">
-                <input type="checkbox" id="f6" class="form-check-input shadow-none me-1">
-                <label class="form-check-label" for="f6">Bantal</label>
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style:"font-size: 18px;">Fasilitas</h5>
+                <div class="mb-2">
+                  <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                  <label class="form-check-label" for="f1">AC</label>
+                </div>
+                <div class="mb-2">
+                  <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
+                  <label class="form-check-label" for="f2">Stopkontak</label>
+                </div>
+                <div class="mb-2">
+                  <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
+                  <label class="form-check-label" for="f4">Wifi</label>
+                </div>
+                <div class="mb-2">
+                  <input type="checkbox" id="f4" class="form-check-input shadow-none me-1">
+                  <label class="form-check-label" for="f5">Selimut</label>
+                </div>
+                <div class="mb-2">
+                  <input type="checkbox" id="f6" class="form-check-input shadow-none me-1">
+                  <label class="form-check-label" for="f6">Bantal</label>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
 
-    <div class="col-lg-9 col-md-12 px-4">
+      <div class="col-lg-9 col-md-12 px-4">
 
-    <?php
-            $class_res =select("SELECT * FROM `class` WHERE `status`=? AND `removed`=?",[1,0],'ii');
+        <?php
+        $class_res = select("SELECT * FROM `class` WHERE `status`=? AND `removed`=?", [1, 0], 'ii');
 
-           while($class_data = mysqli_fetch_assoc($class_res))
-           {
-                // get features of class
+        while ($class_data = mysqli_fetch_assoc($class_res)) {
+          // get features of class
 
-                $fea_q = mysqli_query($con,"SELECT f.name FROM `features` f 
+          $fea_q = mysqli_query($conn, "SELECT f.name FROM `features` f 
                 INNER JOIN `class_features` rfea ON f.id = rfea.features_id 
                 WHERE rfea.class_id = '$class_data[id]'");
 
-                $features_data = "";
-                 while($fea_row = mysqli_fetch_assoc($fea_q)){
-                    $features_data .= "<span class='badge rounded-pill bg-light text-dark text-wrap'>
+          $features_data = "";
+          while ($fea_row = mysqli_fetch_assoc($fea_q)) {
+            $features_data .= "<span class='badge rounded-pill bg-light text-dark text-wrap'>
                      $fea_row[name]
                     </span>";
+          }
+          // get facilities of class
 
-                   
-                 }
-                // get facilities of class
-
-                $fac_q = mysqli_query($con,"SELECT f.name FROM `facilities` f 
+          $fac_q = mysqli_query($conn, "SELECT f.name FROM `facilities` f 
                 INNER JOIN `class_facilities` rfac ON f.id = rfac.facilities_id 
                 WHERE rfac.class_id = '$class_data[id]'");
 
-                $facilities_data = "";
-                while($fac_row = mysqli_fetch_assoc($fac_q)){
-                    $facilities_data .= "<span class='badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base'>
+          $facilities_data = "";
+          while ($fac_row = mysqli_fetch_assoc($fac_q)) {
+            $facilities_data .= "<span class='badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base'>
                      $fac_row[name]
                     </span>";
-                 }
+          }
 
-                //get thumbnail of image
+          //get thumbnail of image
 
-                $class_thumb = CLASS_IMG_PATH."thumbnail.jpg";
-                $thumb_q = mysqli_query($con,"SELECT * FROM `class_images`
+          $class_thumb = CLASS_IMG_PATH . "thumbnail.jpg";
+          $thumb_q = mysqli_query($conn, "SELECT * FROM `class_images`
                  WHERE `class_id` = '$class_data[id]' 
                  AND `thumb` ='1' ");
 
-                 if(mysqli_num_rows($thumb_q)>0){
-                    $thumb_res = mysqli_fetch_assoc($thumb_q);
-                    $class_thumb = CLASS_IMG_PATH.$thumb_res['image'];
-                 }
+          if (mysqli_num_rows($thumb_q) > 0) {
+            $thumb_res = mysqli_fetch_assoc($thumb_q);
+            $class_thumb = CLASS_IMG_PATH . $thumb_res['image'];
+          }
 
 
-                 $book_btn = "";
+          $book_btn = "";
 
-                 if(!$settings_r['shutdown']){
-                    $login=0;
-                    if(isset($_SESSION['login']) && $_SESSION['login']==true){
-                      $login=1;
-                    }
-                    $book_btn = "<button onclick='checkLoginToBook($login,$class_data[id])' class='btn btn-sm w-100 text-white custom-bg shadow-none mb-2'>Book Now</button>";
+          if (!isset($settings_r['shutdown'])) {
+            $login = 0;
+            if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
+              $login = 1;
+            }
+            $book_btn = "<button onclick='checkLoginToBook($login,$class_data[id])' class='btn btn-sm w-100 text-white custom-bg shadow-none mb-2'>Book Now</button>";
+          }
 
-                 }
+          // print class card
 
-                 // print class card
-
-                 echo<<<data
+          echo <<<data
 
                   <div class="card mb-4 border-0 shadow">
                     <div class="row g-0 p-3 align-items-center">
@@ -339,13 +337,14 @@
                   </div>
                             
                   data;
-            }
-          ?>
+        }
+        ?>
+      </div>
     </div>
   </div>
-</div>
 
-<?php require('views/footer.php'); ?>
+  <?php require('views/footer.php'); ?>
 
 </body>
+
 </html>
